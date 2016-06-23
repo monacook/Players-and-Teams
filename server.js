08b47d343556b255e9a14a74cb173fcb9a475e70
monacook <cook.mona@gmail.com>
2016-06-23 16:28:21 -0700

    modified some changes and added more comments

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var path = require("path");
 // create the express app
 var app = express();
 // static content
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, "./client")));
 // setting up ejs and our views folder
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, './views'));
+// app.set('view engine', 'ejs');
 // root route to render the index.ejs view
-app.get('/', function(req, res) {
- res.render("index");
-})
-// post route for adding a user
-app.post('/users', function(req, res) {
- console.log("POST DATA", req.body);
- // This is where we would add the user to the database
- // Then redirect to the root route
- res.redirect('/');
-})
+// app.get('/', function(req, res) {
+//  res.render("index");
+// })
+// // post route for adding a user
+// app.post('/users', function(req, res) {
+//  console.log("POST DATA", req.body);
+//  // This is where we would add the user to the database
+//  // Then redirect to the root route
+//  res.redirect('/');
+// })
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
  console.log("listening on port 8000");
